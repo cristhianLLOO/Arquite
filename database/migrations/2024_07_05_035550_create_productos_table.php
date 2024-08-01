2a96d636b3d71e56b47c567978333d2f13173f65
Erick07-dev <michellerik96@gmail.com>
2024-07-31 20:35:33 -0600

    Add files via upload

---
+++ database/migrations/2024_07_05_035550_create_productos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+// database/migrations/xxxx_xx_xx_create_productos_table.php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->decimal('precio', 8, 2);
+            $table->integer('cantidad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
---
+++ database/migrations/2024_07_29_050219_create_usuarios_table.php
@@ -0,0 +1,24 @@
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuariosTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('correoelectronico')->unique();
+            $table->string('telefono');
+            $table->string('contraseña');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
